e98368fcc27bbf4f0426679de28ea8579c3a48fa
Brien Thomas <thomasbrien94@gmail.com>
2022-07-27 17:28:21 -0700

    Wrote out my test first, I promise Julie don't fail me, forgot to commit before writing my getById function, now attaching the rest of the routing logic to the users.js under controllers folder

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,7 +7,7 @@ describe('user information routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  
+
   it('/users should return a list of users first names and emails', async () => {
     const response = await request(app).get('/users');
     expect(response.body.length).toEqual(50);
@@ -17,6 +17,18 @@ describe('user information routes', () => {
     });
   });
 
+  it('/users:id should return a specific users details', async () => {
+    const response = await request(app).get('/users/50');
+    const tatum = {
+      id: 50,
+      first_name: 'Tatum',
+      last_name: 'Louca',
+      email: 'tlouca1d@senate.gov',
+      gender: 'Female',
+      ip_address: 40.150.247.89,
+    };
+    expect(response.body).toEqual(tatum);
+  });
 
   afterAll(() => {
     pool.end();
