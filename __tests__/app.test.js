5b6b48092954fdd44f691920eb647ef4d35cf7e1
Brien Thomas <thomasbrien94@gmail.com>
2022-07-27 17:08:01 -0700

    filled out the UserInfo model to construct the rows of data from the setup.sql, the controller file users.js takes that data and pulls out the first_name and email for each user, then app.js routes to it at the /users route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,21 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('user information routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  
+  it('/users should return a list of users first names and emails', async () => {
+    const response = await request(app).get('/users');
+    expect(response.body.length).toEqual(50);
+    expect(response.body[7]).toEqual({
+      first_name: 'Opalina',
+      email: 'owale7@tiny.cc',
+    });
   });
+
+
   afterAll(() => {
     pool.end();
   });
